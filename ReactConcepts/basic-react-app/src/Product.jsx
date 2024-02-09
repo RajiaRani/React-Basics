289d41d48938eb551e2a2e18b65866d10d4dd50b
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-09 00:12:19 -0600

    added

--- ReactConcepts/basic-react-app/src/Product.jsx
+++ ReactConcepts/basic-react-app/src/Product.jsx
@@ -1,9 +1,13 @@
 import  "./Product.css";
-function Product({title, description}) {
+function Product({title, description,price, author}) {
+    
     return (
       <div className="Product">
          <h1>{title}</h1>
          <p>{description}</p>
+         <p>{price}</p>
+        <p>{author}</p>
+         
       </div>
     )
 }
--- ReactConcepts/basic-react-app/src/ProductTab.jsx
+++ ReactConcepts/basic-react-app/src/ProductTab.jsx
@@ -1,13 +1,16 @@
 import Product from "./Product";
 function ProductTab() {
+    let authors = ["Rajia Rani", "chenchu reddy", "shiva reddy", "geet"];
+    // let authors2 = { a:"rajia rani", b: "checnhu reddy", c:"shiva reddy", d:"geet" };
     return (
         <>
-        <Product title="God is beautiful!!" description="just faith in it" price={200}/>
-        <Product title="Word is beautiful!!" description="just faith in it" price={200}/>
-        <Product title="Mother is beautiful!!" description="just faith in it" price={200}/>
-        <Product title="Hardwork is beautiful!!" description="just faith in it" price={20000}/>
+        <Product title="God is beautiful!!" description="just faith in it" price={200/2} author={"Rajia Rani"} />
+        <Product title="Word is beautiful!!" description="just faith in it" price={200*3} author={"Checnhu Reddy"}/>
+        <Product title="Mother is beautiful!!" description="just faith in it" price={200-10} author={"Shiva Reddy"}/>
+        <Product title="Hardwork is beautiful!!" description="just faith in it" price={20000+1233} author={"Geet Kaur"}/>
         </>
     )
 }
 export default ProductTab;
-//Note- Number/digits ko hum strings mein pass nhi karna hai humko curly braces {} use karni hogi
+//Note- Number/digits ko hum strings mein pass nhi karna hai humko curly braces {} use karni hogi 
+//hum number ki default value bhi set kar sakte hai
