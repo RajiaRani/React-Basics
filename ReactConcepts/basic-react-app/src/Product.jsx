71feecea2250b09fb97033be8b513456250322a6
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-08 23:38:23 -0600

    added

---
+++ ReactConcepts/basic-react-app/src/Product.jsx
@@ -0,0 +1,10 @@
+import  "./Product.css";
+function Product() {
+    return (
+      <div className="Product">
+         <h1>Title</h1>
+         <p>Description</p>
+      </div>
+    )
+}
+export default Product;
