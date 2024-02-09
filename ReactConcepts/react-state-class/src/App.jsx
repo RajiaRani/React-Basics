30286f41998a95c9248194fec35d1757862c2c5b
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-09 17:06:07 -0600

    added likebutton file

--- ReactConcepts/react-state-class/src/App.jsx
+++ ReactConcepts/react-state-class/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Counter from './Counter'
+import LikeButton from './LikeButton';
 function App() {
 
   return (
     <>
       <h1>States in React</h1>
-      <Counter/>
+      {/* <Counter/> */}
+      <LikeButton/>
     </>
   )
 }
