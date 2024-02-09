796f120eccff1496aed25370857fa53111733baa
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-09 14:18:25 -0600

    added

--- ReactConcepts/basic-react-app/src/Button.jsx
+++ ReactConcepts/basic-react-app/src/Button.jsx
@@ -1,11 +1,16 @@
 function printHello() {
     console.log("Hello");
 }
+function printBye(){
+    console.log("Bye Bye!!");
+}
 
 function Button() {
  return (
     <div>
+
      <button onClick={printHello}>Click Me</button>
+      <h5 onClick={printBye}>Bye</h5>
     </div>
  )
 }
