b707cdc9fbfd82d8723025530dc07373d29416ea
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-09 14:26:04 -0600

    added

--- ReactConcepts/basic-react-app/src/Button.jsx
+++ ReactConcepts/basic-react-app/src/Button.jsx
@@ -7,6 +7,9 @@ function handleClick(){
 function handleMouseOver() {
     console.log("Say something");
 }
+function handleDoubleClick() {
+    console.log("this is double Clicked event");
+}
 function Button() {
  return (
     <div>
@@ -14,6 +17,7 @@ function Button() {
      <button onClick={handleClick}>Click Me</button>
       <h5 onClick={handleClick}>Bye</h5>
       <p onMouseOver={handleMouseOver}>Hello everyone my name is Rajia Rani ....</p>
+      <button onDoubleClick={handleDoubleClick}>Double times click me</button>
     </div>
  )
 }
