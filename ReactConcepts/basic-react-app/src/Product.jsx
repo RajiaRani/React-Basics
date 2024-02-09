8b0509d7901da428b6df2490dfcf6b64648bccd2
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-09 00:44:35 -0600

    added

--- ReactConcepts/basic-react-app/src/Product.jsx
+++ ReactConcepts/basic-react-app/src/Product.jsx
@@ -1,25 +1,15 @@
 import  "./Product.css";
 function Product({title, description,price}) {
     // let list = author.map((author_name) => <li>{author_name}</li>);
-    if(price > 500) {
     return (
       <div className="Product">
          <h1>{title}</h1>
          <p>{description}</p>
          <p>{price}</p>
-         <p>Discount of 10%</p>
+         {price>500 && <p>Discount of 10%</p>}
+         {price<500 && <a href="/">Get the discount</a>}
          
       </div>
     )
-    } else {
-        return (
-            <div className="Product">
-               <h1>{title}</h1>
-               <p>{description}</p>
-               <p>{price}</p>
-            </div>
-          )
-    }
-
 }
 export default Product;
