41c44362417906329f4f2f3b04a990c964e3582e
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-09 17:20:08 -0600

    added likebutton file

--- ReactConcepts/react-state-class/src/LikeButton.jsx
+++ ReactConcepts/react-state-class/src/LikeButton.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react"
 
 export default function LikeButton() {
+    let [isLiked, setIsLiked] = useState(false);
+    let [countLike, setCountLike] = useState(0);
+
+    let toggleLike = () => {
+        setIsLiked(!isLiked);
+        setCountLike(countLike+1);
+        console.log(isLiked)
+    }
+    let likeStyle = {color:"red"};
+
+
     return (
         <div>
-            <i class="fa-regular fa-heart"></i>
+        <p>Likes = {countLike}</p>
+         <p onClick={toggleLike}>
+            {isLiked ? (<i className="fa-solid fa-heart" style={likeStyle}></i>) :
+            (<i className="fa-regular fa-heart"></i>)}
+         </p>
         </div>
     )
 }
