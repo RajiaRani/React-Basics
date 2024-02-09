6bc735a1e7d5ca277887d924989b02e83e55b97f
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-08 23:45:25 -0600

    added

--- ReactConcepts/basic-react-app/src/ProductTab.jsx
+++ ReactConcepts/basic-react-app/src/ProductTab.jsx
@@ -2,11 +2,12 @@ import Product from "./Product";
 function ProductTab() {
     return (
         <>
-        <Product title="God is beautiful!!" description="just faith in it"/>
-        <Product title="Word is beautiful!!" description="just faith in it"/>
-        <Product title="Mother is beautiful!!" description="just faith in it"/>
-        <Product title="Hardwork is beautiful!!" description="just faith in it"/>
+        <Product title="God is beautiful!!" description="just faith in it" price={200}/>
+        <Product title="Word is beautiful!!" description="just faith in it" price={200}/>
+        <Product title="Mother is beautiful!!" description="just faith in it" price={200}/>
+        <Product title="Hardwork is beautiful!!" description="just faith in it" price={20000}/>
         </>
     )
 }
 export default ProductTab;
+//Note- Number/digits ko hum strings mein pass nhi karna hai humko curly braces {} use karni hogi
