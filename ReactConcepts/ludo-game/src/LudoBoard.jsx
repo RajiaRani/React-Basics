424ff81527322f35523144a533a089cadcd6c701
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-10 10:42:38 -0600

    added

---
+++ ReactConcepts/ludo-game/src/LudoBoard.jsx
@@ -0,0 +1,13 @@
+
+export default function ludoBoard() {
+    return (
+        <div>
+            <p>Game Begins!</p>
+            <div className="board">
+                <p>Blue Moves</p>
+                <button>+1</button>
+            </div>
+        </div>
+        
+    )
+}
