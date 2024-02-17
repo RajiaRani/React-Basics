2fdcad9453bd5f5a98913a10b226c8952c981dad
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-16 22:18:31 -0600

    added

--- ReactConcepts/lottery-game/src/lottery.jsx
+++ ReactConcepts/lottery-game/src/lottery.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
+import "./lottery.css";
+import { genTicket } from "./helper";
 
 export default function Lottery() {
-    let [ticket, setTicket] = useState([0,0,0]);
+    let [ticket, setTicket] = useState(genTicket(3));
     return (
         <div>
-            <h2>Lottery Game</h2>
+            <h1>Lottery Game!</h1>
             <div className="ticket">
                 <span>{ticket[0]}</span>
                 <span>{ticket[1]}</span>
