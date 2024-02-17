5bcf91872a701d07aa026f54a1b6af3148131a76
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-16 22:07:00 -0600

    added

--- ReactConcepts/lottery-game/src/lottery.jsx
+++ ReactConcepts/lottery-game/src/lottery.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
 export default function Lottery() {
+    let [ticket, setTicket] = useState([0,0,0]);
     return (
         <div>
             <h2>Lottery Game</h2>
+            <div className="ticket">
+                <span>{ticket[0]}</span>
+                <span>{ticket[1]}</span>
+                <span>{ticket[2]}</span>
+            </div>
         </div>
     )
 }
