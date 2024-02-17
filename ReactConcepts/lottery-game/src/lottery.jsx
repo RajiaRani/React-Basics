b7a70471eae0a503a8863612dbc9401bd0db5888
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-16 23:40:50 -0600

    added lottery game

--- ReactConcepts/lottery-game/src/lottery.jsx
+++ ReactConcepts/lottery-game/src/lottery.jsx
@@ -5,6 +5,11 @@ import { genTicket , sum} from "./helper";
 export default function Lottery() {
     let [ticket, setTicket] = useState(genTicket(3));
     let isWinning = sum(ticket) == 20;
+
+    let buyTicket = () => {
+        setTicket(genTicket(3));
+    };
+
     return (
         <div>
             <h1>Lottery Game!</h1>
@@ -13,6 +18,8 @@ export default function Lottery() {
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
+            <br/>
+            <button onClick={buyTicket} style={{backgroundColor:"grey", color:"white"}}>Buy new ticket</button>
             <h2>{ isWinning && "Congralutions, you won!!"}</h2>
         </div>
     )
