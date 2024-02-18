03990a5a342489fcbe0ef53587e9d1525d4cbfd2
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 00:42:13 -0600

    added comments form

--- ReactConcepts/form-project/src/CommentsForm.jsx
+++ ReactConcepts/form-project/src/CommentsForm.jsx
@@ -7,13 +7,53 @@ export default function CommentsForm() {
         rating:""
     });
 
+    let handleInputChange = (event) => {
+      setFormData((currData) => {
+        return {...currData, [event.target.name]:event.target.value};
+      });
+    };
+
+    let handleSubmit = (event) =>{
+        console.log(formData);
+        event.preventDefault({
+            username:"",
+            remarks:"",
+            rating:""
+        });
+    }
+
     return (
         <div>
             <h4>Give a Comment:</h4>
             <form action="">
-                <input type="text" placeholder="username"  value={formData.username}/> <br /> <br />
-                <textarea placeholder="remarks add few" value={formData.remarks}>Remarks</textarea>  <br /> <br />
-                <input type="number" placeholder="rating"  min={1} max={5} value={formData.rating}/>  <br /> <br />
+                <label htmlFor="username"></label>
+                <input 
+                type="text" 
+                placeholder="username"  
+                value={formData.username}
+                id="username"
+                onChange={handleInputChange}
+                onSubmit={handleSubmit}
+                /> <br /> <br />
+
+                <label htmlFor="remarks"></label>
+                <textarea 
+                placeholder="remarks add few" 
+                value={formData.remarks}
+                id="remarks"
+                onChange={handleInputChange}
+                onSubmit={handleSubmit}
+                ></textarea>  <br /> <br />
+
+                <label htmlFor="rating"></label>
+                <input type="number" 
+                placeholder="rating" 
+                 min={1} max={5} 
+                 value={formData.rating}
+                 id="rating"
+                 onChange={handleInputChange}
+                 onSubmit={handleSubmit}
+                 />  <br /> <br />
                 <button>Add Comment</button>
             </form>
         </div>
