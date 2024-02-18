ce45019c787eade0cfa95e7997cc75c31245dcf9
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 12:22:49 -0600

    added new mini-project based on React

--- ReactConcepts/mini-project-react/src/SearchBox.jsx
+++ ReactConcepts/mini-project-react/src/SearchBox.jsx
@@ -1,7 +1,12 @@
+import TextField from '@mui/material/TextField';
+
 export  default function SearchBox() {
     return (
         <div>
             <h3>Search For the Weather</h3>
+            <form>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            </form>
         </div>
     )
 }
