e732f5c644c9d71f01441db1b6231125e5afb14e
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-17 21:44:35 -0600

    added

---
+++ ReactConcepts/lottery-game/src/button.jsx
@@ -0,0 +1,3 @@
+export default function Button({action}) {
+    return  <button onClick={action} style={{backgroundColor:"green", color:"white"}}>Buy new ticket</button>
+}
--- ReactConcepts/lottery-game/src/lottery.jsx
+++ ReactConcepts/lottery-game/src/lottery.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { genTicket , sum} from "./helper";
 import Ticket from "./ticket";
+import Button from "./button";
 
 export default function Lottery({n=3,  winCondition}) {
     let [ticket, setTicket] = useState(genTicket(n));
@@ -14,7 +15,7 @@ export default function Lottery({n=3,  winCondition}) {
         <div>
             <h1>Lottery Game!</h1>
             <Ticket ticket={ticket}/>
-            <button onClick={buyTicket} style={{backgroundColor:"green", color:"white"}}>Buy new ticket</button>
+            <Button action={buyTicket}/ >
             <h2>{ isWinning && "Congralutions, you won!!"}</h2>
         </div>
     )
