a46094c833aa64c858a8cd3e424cacb434667148
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 01:29:17 -0600

    added

--- ReactConcepts/form-project/src/Joker.jsx
+++ ReactConcepts/form-project/src/Joker.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export default function Jocker(){
+    let [joke, setJoke] = useState({});
+
+    const URL = "https://official-joke-api.appspot.com/random_joke";
+
+    const getNewJoke = async() => {
+     let response = await fetch(URL);
+     let jsonResponse = await response.json();
+     console.log(jsonResponse);
+     setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+    };
+
     return (
         <div>
             <h2>Joker!</h2>
+            <h2>{joke.setup}</h2>
+            <h2>{joke.punchline}</h2>
+            <button onClick={getNewJoke}>New Joke</button>
         </div>
     )
 }
