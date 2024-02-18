aa77e922f2e671f8623a0d60594e9aae187ecbbf
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-17 23:47:28 -0600

    added the new form method

---
+++ ReactConcepts/form-project/src/form.jsx
@@ -0,0 +1,9 @@
+export default function Form() {
+  return (
+    <form>
+        <input type="text" placeholder="Enter your full name" />
+        <input type="number" placeholder="Enter your roll number" />
+        <button>Submit</button>
+    </form>
+  )
+}
