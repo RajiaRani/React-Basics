467e0855d3cc3c80aedd2de8b27b8eedd4678ee5
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 14:45:26 -0600

    added new mini-project based on React

--- ReactConcepts/mini-project-react/src/SearchBox.jsx
+++ ReactConcepts/mini-project-react/src/SearchBox.jsx
@@ -5,26 +5,31 @@ import { useState } from 'react';
 
 export default function SearchBox({updateInfo}) {
     let [city, setCity] = useState("");
+    let [error, setError] = useState("");
 
    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
    const API_KEY = "f235faf4ac26c447756eca2dd895401e";
-
-  let getWeatherInfo = async() => {
-   let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
-   let jsonResponse = await response.json();
-   //console.log(jsonResponse);
-
-   let result = {
-    city:city,
-    temp: jsonResponse.main.temp,
-    tempMin : jsonResponse.main.temp_min,
-    tempMax : jsonResponse.main.temp_max,
-    humidity: jsonResponse.main.humidity,
-    feelslike: jsonResponse.main.feels_like,
-    weather: jsonResponse.weather[0].description,
-   };
-   console.log(result)
-   return result;
+  
+   let getWeatherInfo = async() => {
+  try{
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse = await response.json();
+        //console.log(jsonResponse);
+     
+        let result = {
+         city:city,
+         temp: jsonResponse.main.temp,
+         tempMin : jsonResponse.main.temp_min,
+         tempMax : jsonResponse.main.temp_max,
+         humidity: jsonResponse.main.humidity,
+         feelslike: jsonResponse.main.feels_like,
+         weather: jsonResponse.weather[0].description,
+        };
+        console.log(result)
+        return result;
+  }catch(err) {
+     throw err;
+  }
 
   };
 
@@ -33,11 +38,15 @@ export default function SearchBox({updateInfo}) {
     };
 
     let handleSubmit =  async(event) => {
-       event.preventDefault();
+       try{
+        event.preventDefault();
        console.log(city);
        setCity(" ");
        let newInfo = await getWeatherInfo();
        updateInfo(newInfo);
+       } catch(err) {
+        setError("No such place in Our API");
+       }
     };
 
     return (
