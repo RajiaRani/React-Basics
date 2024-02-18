572601b881ae028160fb78d40dd3c1adc56c0c06
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-17 23:52:23 -0600

    added form

--- ReactConcepts/form-project/src/form.jsx
+++ ReactConcepts/form-project/src/form.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 export default function Form() {
+  let [fullName, setFullName] = useState("rajia");
+
+  let hanadleNameChange=(event) => {
+    setFullName(event.target.value);
+  };
+
   return (
     <form>
-        <input type="text" placeholder="Enter your full name" />
+        <input type="text" placeholder="Enter your full name"  value={fullName} onChange={hanadleNameChange}/>
+        <br /> <br />
         <input type="number" placeholder="Enter your roll number" />
+        <br /> <br />
         <button>Submit</button>
     </form>
   )
