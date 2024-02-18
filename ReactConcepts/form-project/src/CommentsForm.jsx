e6d97a38600496998dc868708b0ca6758a41a389
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 00:31:41 -0600

    added comments form

--- ReactConcepts/form-project/src/CommentsForm.jsx
+++ ReactConcepts/form-project/src/CommentsForm.jsx
@@ -2,6 +2,11 @@ export default function CommentsForm() {
     return (
         <div>
             <h4>Give a Comment:</h4>
+            <form action="">
+                <input type="text" placeholder="username" /> <br /> <br />
+                <textarea>Remarks</textarea>  <br /> <br />
+                <input type="number" placeholder="rating" />  <br /> <br />
+            </form>
         </div>
     )
 }
