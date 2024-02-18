4bf27e9783777b9100968fbadbbeeb44f7255d04
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 12:36:46 -0600

    added new mini-project based on React

--- ReactConcepts/mini-project-react/src/SearchBox.jsx
+++ ReactConcepts/mini-project-react/src/SearchBox.jsx
@@ -1,11 +1,35 @@
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './SearchBox.css';
+import { useState } from 'react';
 
-export  default function SearchBox() {
+export default function SearchBox() {
+
+    let [city, setCity] = useState("");
+
+    let handleChange = (event) => {
+      setCity(event.target.value);
+    };
+
+    let handleSubmit = (event) => {
+       event.preventDefault();
+       console.log(city);
+       setCity(" ");
+    };
+
     return (
-        <div>
+        <div className='SearchBox'>
             <h3>Search For the Weather</h3>
-            <form>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <form onSubmit={handleSubmit}>
+                <TextField 
+                id="city" 
+                label="City Name" 
+                variant="outlined" 
+                value={city}
+                onChange={handleChange}
+                required />
+                <br /> <br />
+                <Button variant="contained" type='submit' > Search </Button>
             </form>
         </div>
     )
