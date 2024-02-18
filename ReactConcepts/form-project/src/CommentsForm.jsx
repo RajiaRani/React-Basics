7c3814522168e99ed1f87fdf0aef47ef06d437f0
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 00:35:44 -0600

    added comments form

--- ReactConcepts/form-project/src/CommentsForm.jsx
+++ ReactConcepts/form-project/src/CommentsForm.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
+
 export default function CommentsForm() {
+    let [formData, setFormData] = useState ({
+        username:"",
+        remarks:"",
+        rating:""
+    });
+
     return (
         <div>
             <h4>Give a Comment:</h4>
             <form action="">
-                <input type="text" placeholder="username" /> <br /> <br />
-                <textarea>Remarks</textarea>  <br /> <br />
-                <input type="number" placeholder="rating"  min={1} max={5}/>  <br /> <br />
+                <input type="text" placeholder="username"  value={formData.username}/> <br /> <br />
+                <textarea placeholder="remarks add few" value={formData.remarks}>Remarks</textarea>  <br /> <br />
+                <input type="number" placeholder="rating"  min={1} max={5} value={formData.rating}/>  <br /> <br />
                 <button>Add Comment</button>
             </form>
         </div>
