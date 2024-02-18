8a81f7cce6678c6f019562e6bff20e3e30d869f1
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 01:17:53 -0600

    added new counter file

--- ReactConcepts/form-project/src/App.jsx
+++ ReactConcepts/form-project/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import CommentsForm from './CommentsForm'
 import Counter from './Counter'
+import Jocker from './Joker'
 import Form from './form'
 
 function App() {
 return (
  <>
-   {/* <Form/> 
-  <CommentsForm/> */}
-  <Counter/>
+    {/* <Form/> 
+  <CommentsForm/> 
+  <Counter/> */}
+  <Jocker/>
  </>
 )
 }
---
+++ ReactConcepts/form-project/src/Joker.jsx
@@ -0,0 +1,7 @@
+export default function Jocker(){
+    return (
+        <div>
+            <h2>Joker!</h2>
+        </div>
+    )
+}
