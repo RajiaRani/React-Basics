189e992778d7f4ad189630c715b2fe8666558ca5
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 15:14:18 -0600

    added new mini-project based on React

--- ReactConcepts/mini-project-react/src/InfoBox.jsx
+++ ReactConcepts/mini-project-react/src/InfoBox.jsx
@@ -2,6 +2,10 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import CloudIcon from '@mui/icons-material/Cloud';
+
 export default function InfoBox({info}) {
     const INIT_URL='https://images.unsplash.com/photo-1572687413625-cb2c4d9c4d32?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
     const HOT_URL="https://images.unsplash.com/photo-1466611653911-95081537e5b7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
@@ -20,7 +24,13 @@ export default function InfoBox({info}) {
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                       {info.city}
+                       {info.city} {
+                        info.humidity > 80 
+                        ? <ThunderstormIcon/> 
+                        : info.temp> 15 
+                        ? <WbSunnyIcon/> 
+                        : <CloudIcon/>
+                       }
                     </Typography>
                     <Typography variant="body2" color="text.secondary" component={"span"}>
                         <div>Temperature = {info.temp}&deg;C</div>
