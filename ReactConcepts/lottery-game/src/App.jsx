873d7d6ee55e1f42a854a5134caefdd7d0e1d2da
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-17 21:16:23 -0600

    added new features

--- ReactConcepts/lottery-game/src/App.jsx
+++ ReactConcepts/lottery-game/src/App.jsx
@@ -6,9 +6,10 @@ import Ticket from './ticket'
 function App() {
    return (
    <>
-   <Ticket ticket={[0,1,2]} />
+   <Lottery n={3} winningSum={25}/>
+   {/* <Ticket ticket={[0,1,2]} />
    <Ticket ticket={[2,4,6,8,9]} />
-   <Ticket ticket={[0,2,7,9,19, 22]} />
+   <Ticket ticket={[0,2,7,9,19, 22]} /> */}
    </>
    );
 }
--- ReactConcepts/lottery-game/src/lottery.jsx
+++ ReactConcepts/lottery-game/src/lottery.jsx
@@ -1,24 +1,18 @@
 import { useState } from "react"
-import "./lottery.css";
 import { genTicket , sum} from "./helper";
 
-export default function Lottery() {
-    let [ticket, setTicket] = useState(genTicket(3));
-    let isWinning = sum(ticket) == 20;
+export default function Lottery({n,  winningSum}) {
+    let [ticket, setTicket] = useState(genTicket(n));
+    let isWinning = sum(ticket) == winningSum;
 
     let buyTicket = () => {
-        setTicket(genTicket(3));
+        setTicket(genTicket(n));
     };
 
     return (
         <div>
             <h1>Lottery Game!</h1>
-            <div className="ticket">
-                 <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span> 
-            </div>
-            <br/>
+            <Ticket ticket={ticket}/>
             <button onClick={buyTicket} style={{backgroundColor:"grey", color:"white"}}>Buy new ticket</button>
             <h2>{ isWinning && "Congralutions, you won!!"}</h2>
         </div>
