d6416e52db901e3496feac0d469802bc72cf50cb
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-17 21:32:04 -0600

    added

--- ReactConcepts/lottery-game/src/App.jsx
+++ ReactConcepts/lottery-game/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Lottery from './lottery'
+import { genTicket , sum} from "./helper";
 
+let winCondition = (ticket) => {
+   return sum(ticket) === 10;
+}
 
 
 function App() {
    return (
    <>
-   <Lottery n={3} winningSum={25}/>
+   <Lottery n={3} winCondition={winCondition}/>
    </>
    );
 }
--- ReactConcepts/lottery-game/src/lottery.jsx
+++ ReactConcepts/lottery-game/src/lottery.jsx
@@ -2,9 +2,9 @@ import { useState } from "react"
 import { genTicket , sum} from "./helper";
 import Ticket from "./ticket";
 
-export default function Lottery({n=3,  winningSum=20}) {
+export default function Lottery({n=3,  winCondition}) {
     let [ticket, setTicket] = useState(genTicket(n));
-    let isWinning = sum(ticket) == winningSum;
+    let isWinning = winCondition(ticket);
 
     let buyTicket = () => {
         setTicket(genTicket(n));
