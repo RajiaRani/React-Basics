6019ae2dad118d1a58124f0a7028f18bcf044ed7
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 01:37:53 -0600

    added new counter file

--- ReactConcepts/form-project/src/Joker.jsx
+++ ReactConcepts/form-project/src/Joker.jsx
@@ -1,16 +1,26 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
-export default function Jocker(){
+export default function Jocker() {
+
     let [joke, setJoke] = useState({});
 
     const URL = "https://official-joke-api.appspot.com/random_joke";
 
-    const getNewJoke = async() => {
-     let response = await fetch(URL);
-     let jsonResponse = await response.json();
-     console.log(jsonResponse);
-     setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+    const getNewJoke = async () => {
+        let response = await fetch(URL);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
     };
+
+    useEffect(() => { async function getFirstJoke() {
+        let response = await fetch(URL);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+    } 
+   getFirstJoke();}, []);
 
     return (
         <div>
