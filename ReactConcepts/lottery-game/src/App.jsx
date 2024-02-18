9fcb10d54c641e304092ac7b4a4b114b42b428cb
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-17 21:45:52 -0600

    added

--- ReactConcepts/lottery-game/src/App.jsx
+++ ReactConcepts/lottery-game/src/App.jsx
@@ -4,7 +4,8 @@ import { genTicket , sum} from "./helper";
 
 let winCondition = (ticket) => {
    // return sum(ticket) === 10;
-   return ticket.every((num) => num === ticket[0]);
+   // return ticket.every((num) => num === ticket[0]);
+   return ticket[0] === 0;
 };
 
 
