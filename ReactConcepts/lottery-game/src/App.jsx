7947660c86e186f01303622ee8cbd714de796f20
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-17 20:47:07 -0600

    added

--- ReactConcepts/lottery-game/src/App.jsx
+++ ReactConcepts/lottery-game/src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css'
 import Lottery from './lottery'
+import TicketNumber from './ticketNum'
+
 
 function App() {
-   return <Lottery/>
+   return 
+   <>
+   <TicketNumber num={5} />
+   <TicketNumber num={3} />
+   <TicketNumber num={2} />
+   </>
 }
 
 export default App
---
+++ ReactConcepts/lottery-game/src/ticketNum.jsx
@@ -0,0 +1,3 @@
+export default function TicketNumber({num}) {
+  return (<span>{num}</span>)
+}
