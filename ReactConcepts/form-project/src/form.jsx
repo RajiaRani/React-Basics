92ea29472c7fe3ca150d31f0f87cde70b4503528
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-17 23:55:03 -0600

    added form

--- ReactConcepts/form-project/src/form.jsx
+++ ReactConcepts/form-project/src/form.jsx
@@ -9,7 +9,13 @@ export default function Form() {
 
   return (
     <form>
-        <input type="text" placeholder="Enter your full name"  value={fullName} onChange={hanadleNameChange}/>
+        <label htmlFor="usernmae">Full legal name:</label>
+        <input type="text" 
+        placeholder="Enter your full name"  
+        value={fullName} 
+        onChange={hanadleNameChange}
+        id={username}
+        />
         <br /> <br />
         <input type="number" placeholder="Enter your roll number" />
         <br /> <br />
