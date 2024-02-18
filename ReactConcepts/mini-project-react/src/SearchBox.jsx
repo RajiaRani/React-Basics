5d6a3c9e18a7742178f417e1fde54f6ca6113cec
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-18 12:55:16 -0600

    added new mini-project based on React

--- ReactConcepts/mini-project-react/src/SearchBox.jsx
+++ ReactConcepts/mini-project-react/src/SearchBox.jsx
@@ -4,8 +4,17 @@ import './SearchBox.css';
 import { useState } from 'react';
 
 export default function SearchBox() {
-
     let [city, setCity] = useState("");
+
+   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+   const API_KEY = "f235faf4ac26c447756eca2dd895401e";
+
+  let getWeatherInfo = async() => {
+   let response = await fetch(`${API_URL}? q=${city}&appid=${API_KEY}`);
+   let jsonResponse = await response.json();
+   console.log(jsonResponse);
+
+  };
 
     let handleChange = (event) => {
       setCity(event.target.value);
